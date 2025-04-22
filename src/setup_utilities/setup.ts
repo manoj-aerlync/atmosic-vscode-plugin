0ce4b6d6b8f9e355352d2e9c8d7097e1dc02b545
Manojkumar <manoj@atmosic.com>
2025-04-22 18:05:04 +0530

    West init and West update support for atmosic openair repository
    Signed-off-by: Manojkumar <manoj@atmosic.com>

--- src/setup_utilities/setup.ts
+++ src/setup_utilities/setup.ts
@@ -605,7 +605,7 @@ export async function westInit(context: vscode.ExtensionContext, wsConfig: Works
   } else if (westSelection.path === undefined) {
     cmd = `west init ${westSelection.additionalArgs}`;
   } else {
-    cmd = `west init -l ${westSelection.path} ${westSelection.additionalArgs}`;
+    cmd = `west init -m ${'https://github.com/Atmosic/openair.git'} ${'--mr main'}`;
   }
 
   wsConfig.activeSetupState.zephyrDir = ""
